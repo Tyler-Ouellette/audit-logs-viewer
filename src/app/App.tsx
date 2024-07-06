f5c24aeb67ee3c504a1bf390cfbbb85f37339a90
Tyler Ouellette <ouellette.tyler@hotmail.com>
2024-07-05 18:49:06 -0600

    ADD Settings Logs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,9 @@
 import { Page } from "@dynatrace/strato-components-preview";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { AuditLogs } from "./pages/AuditLogs";
+import { OldAuditLogs } from "./pages/OldAuditLogs";
+import { GatewayLogs } from "./pages/GatewayLogs";
+import { SettingsLogs } from "./pages/SettingsLogs";
 import { Header } from "./components/Header";
 
 export const App = () => {
@@ -12,8 +14,10 @@ export const App = () => {
       </Page.Header>
       <Page.Main>
         <Routes>
-          <Route path="/" element={<AuditLogs />} />
-          <Route path="/auditLogs" element={<AuditLogs />} />
+          <Route path="/" element={<SettingsLogs />} />
+          <Route path="/settingsAuditLogs" element={<SettingsLogs />} />
+          <Route path="/apiGatewayLogs" element={<GatewayLogs />} />
+          <Route path="/oldAuditLogs" element={<OldAuditLogs />} />
         </Routes>
       </Page.Main>
     </Page>
